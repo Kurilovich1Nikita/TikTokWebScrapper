8939b6f7c736776692b94574700efa4b1246bc57
Никита Курилович <nikitakurilovich@MacBook-Air-Nikita.local>
2022-04-07 11:51:04 +0200

    Added functionality for saving data to file

--- main.py
+++ main.py
@@ -26,6 +26,13 @@ class TikTokBot():
 			except FileExistsError:
 				i += 1
 
+	def save_data(self, data_ar):
+		with open(f'{self.username}_profile_data.txt','w') as f:
+			for i in data_ar:
+				f.write(i)
+				f.write('\n')
+		print(f"Profile data saved to {os.getcwd()}")
+
 	def capture_token_signature(self):
 		caps = DesiredCapabilities.CHROME
 		caps['goog:loggingPrefs'] = {'performance': 'ALL'}
@@ -51,32 +58,47 @@ class TikTokBot():
 
 		self.driver.get(url)
 
+		data_ar = []
+
 		st = str(self.driver.page_source)
 		id = st.split("id\":\"", 1)[1].split("\"", 1)[0]
-		print("Id: " + id)
+		data_ar.append("Id: " + id)
+		print(data_ar[0])
 		unique_id = st.split("uniqueId\":\"", 1)[1].split("\"", 1)[0]
-		print("Unique Id: " + unique_id)
+		data_ar.append("Unique Id: " + unique_id)
+		print(data_ar[1])
 		nickname = st.split("nickname\":\"", 1)[1].split("\"", 1)[0]
-		print("Nickname: " + nickname)
+		data_ar.append("Nickname: " + nickname)
+		print(data_ar[2])
 		signature = st.split("signature\":\"", 1)[1].split("\"", 1)[0]
-		print("Signature: " + signature)
+		data_ar.append("Signature: " + signature)
+		print(data_ar[3])
 		verified = st.split("verified\":", 1)[1].split(",", 1)[0]
-		print("Verified: " + verified)
+		data_ar.append("Verified: " + verified)
 		if "bioLink" in st:
 			bio_link = st.split("bioLink\":{\"link\":\"", 1)[1].split("\"", 1)[0]
-			print("Bio Link: " + bio_link)
+			data_ar.append("Bio Link: " + bio_link)
 		private_account = st.split("privateAccount\":", 1)[1].split(",", 1)[0]
-		print("Private account: " + private_account)
+		data_ar.append("Private account: " + private_account)
+		print(data_ar[5])
 		isUnderAge18 = st.split("isUnderAge18\":", 1)[1].split("}", 1)[0]
-		print("Is under age 18: " + isUnderAge18)
+		data_ar.append("Is under age 18: " + isUnderAge18)
+		print(data_ar[6])
 		following_count = st.split("followingCount\":", 1)[1].split(",", 1)[0]
-		print("Following count: " + following_count)
+		data_ar.append("Following count: " + following_count)
+		print(data_ar[7])
 		follower_count = st.split("followerCount\":", 1)[1].split(",", 1)[0]
-		print("Follower count: " + follower_count)
+		data_ar.append("Follower count: " + follower_count)
+		print(data_ar[8])
 		heart_count = st.split("heartCount\":", 1)[1].split(",", 1)[0]
-		print("Heart count: " + heart_count)
+		data_ar.append("Heart count: " + heart_count)
+		print(data_ar[9])
 		video_count = st.split("videoCount\":", 1)[1].split(",", 1)[0]
-		print("Video count: " + video_count)
+		data_ar.append("Video count: " + video_count)
+		print(data_ar[10])
+
+		self.create_dir()
+		self.save_data(data_ar)
 
 		self.driver.quit()
 
